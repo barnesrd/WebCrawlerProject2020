e76dabc426286d99ef08631b4d7f5f265e1455aa
Ryan Barnes <72225971+barnesrd@users.noreply.github.com>
2020-11-18 10:07:13 -0600

    Add files via upload

---
+++ WebCrawlerProject/src/main/java/SpiderTest.java
@@ -0,0 +1,17 @@
+/**
+ * @COSC 2351 Data Structures [Web Crawler Project] 
+ * @author marianky
+ */
+
+public class SpiderTest
+{
+    /**
+     * The main method creates a spider object (which creates spider legs) and crawls the web.
+     * 
+     */
+    public static void main(String[] args)
+    {
+        Spider spider = new Spider();
+        spider.search("http://www.cnn.com", "Cyber");
+    }
+}
